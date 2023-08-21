fbd16a5a68747d075d0461524eb89e75ac4bc5ff
Venkateswara Reddy Challa <vensablue@gmail.com>
2023-08-21 18:50:13 +0530

    Reduce to Zero using Bitwise

---
+++ src/com/venkat/DSAPractice/NumberOfStepsToReduceToZeroBitwise.java
@@ -0,0 +1,25 @@
+	package com.venkat.DSAPractice;
+
+public class NumberOfStepsToReduceToZeroBitwise {
+
+	public static void main(String[] args) {
+		System.out.println(numberOfSteps(123));
+
+	}
+	
+	public static int numberOfSteps(int num) {
+	      int step=0;
+	       while(num > 0){
+	           if((num & 2) == 0){
+	        	   num = num/2;
+	           }
+	           else {
+	        	   num--;
+	           }
+	           step++;
+	       }
+	       return step;
+	    }
+
+
+}
