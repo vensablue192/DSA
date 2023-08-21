e1958a09da103b26b6dfdd2fe33f2575d3546a98
Venkateswara Reddy Challa <vensablue@gmail.com>
2023-08-21 11:07:28 +0530

    richest customer wealth

---
+++ src/RichestCustomerWealth.java
@@ -0,0 +1,20 @@
+public class RichestCustomerWealth{
+    public static void main(String[] args){
+        int[][] wealthInBanks = {{4,5} , {45,43} , {2,5} , {10,12}};
+        int wealth = maximumWealth(wealthInBanks);
+        System.out.println("max welth - " + wealth );
+    }
+
+    public static int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+        for(int[] cashInBank: accounts){
+            int totalCustCash=0;
+            for (int cashInEachBank: cashInBank) {
+                totalCustCash += cashInEachBank; 
+            }
+            maxWealth = Math.max(maxWealth,totalCustCash);
+        }
+        
+        return maxWealth;
+    }
+}
