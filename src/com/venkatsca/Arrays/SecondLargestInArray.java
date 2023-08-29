8f3b822d6c770946c82b62daf9c653e2ae41cdd7
Venkateswara Reddy Challa <vensablue@gmail.com>
2023-08-29 13:51:45 +0530

    Find the second largest from the given array

---
+++ src/com/venkatsca/Arrays/SecondLargestInArray.java
@@ -0,0 +1,38 @@
+/*You are given an integer array A. You have to find the second largest element/value in the array or report that no such element exists.
+ * Return the second largest element. If no such element exist then return -1.
+ * */
+
+package com.venkatsca.Arrays;
+
+import java.util.Arrays;
+
+public class SecondLargestInArray {
+
+	public static void main(String[] args) {
+		int[] A= {4};
+		System.out.println("Second largest " + secondLargest(A));
+
+	}
+	
+	public static int secondLargest(int[] A) {
+		int result=-1;
+		int highest = -1;
+		int secondhighest = -2;
+		if(A.length > 1) {
+		for(int i: A) {
+			if(i > highest) {
+				secondhighest=highest;
+				highest=i;
+			}
+			else if(i > secondhighest && i != highest) {
+				secondhighest=i;
+			}
+		}
+		result=secondhighest;
+	 }
+		return result;	
+
+	}
+	
+
+}
